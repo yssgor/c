60836d96b1bb9cee5505814552b25ce96b9f9fa9
Ygor Silva <83100543+yssgor@users.noreply.github.com>
2022-05-25 23:02:19 -0300

    Add files via upload

---
+++ jogoAdivinhacao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    int numeroSecreto;
+
+    numeroSecreto = 42;
+    
+    printf("\n");
+    printf("*******************************************\n");
+    printf("*   Bem vindo ao jogo de Adivinhação    *\n");
+    printf("*******************************************\n");
+}
